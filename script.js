2cab54ef36a6ad0a86af07673aed95abbe90cbf6
Douglas-Dorug <douglasx30@gmail.com>
2022-06-18 19:44:16 -0300

    Desenvolvida a comparacao entre as cartas

--- script.js
+++ script.js
@@ -1,9 +1,45 @@
 const cards = document.querySelectorAll('.card');
+let hasFlippedCard = false;
+let firstCard, secondeCard;
+let lockBoard = false;
 
 function flipCard(){
-    this.classList.toggle('flip');
+    if(lockBoard) return;
+
+    this.classList.add('flip');
+
+    if(!hasFlippedCard){
+        hasFlippedCard = true;
+        firstCard = this;
+        return;
+    }
+
+    secondeCard = this;
+    hasFlippedCard = false;
+    checkForMath();
+}
+
+function checkForMath(){
+    if(firstCard.dataset.card === secondeCard.dataset.card){
+        disableCards();
+        return;
+    }
+    unflipcard();
+}
+
+function disableCards(){
+    firstCard.removeEventListener('click', flipCard);
+    secondeCard.removeEventListener('click', flipCard);
+}
+
+function unflipcard(){
+    setTimeout(() => {
+        firstCard.classList.remove('flip');
+        secondeCard.classList.remove('flip');
+    }, 2000)
 }
 
 cards.forEach((card) => {
     card.addEventListener('click', flipCard);
 })
+
